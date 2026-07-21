214bc8f8e9b2b7713740bfcc8cec97b0653e14c4
nflx <flux@sys.nu>
2026-07-21 13:25:04 +0200

    Keep pane font scale out of cli list output

--- wezterm/src/cli/list.rs
+++ wezterm/src/cli/list.rs
@@ -138,8 +138,6 @@ struct CliListResultItem {
     left_px: usize,
     /// Number of pixels from the top of the tab area to the top of this pane
     top_px: usize,
-    /// Per-pane font scale override, if set
-    font_scale: Option<f64>,
     tab_title: String,
     window_title: String,
     is_active: bool,
@@ -164,7 +162,6 @@ impl CliListResultItem {
             top_px,
             is_active_pane,
             is_zoomed_pane,
-            font_scale,
             tty_name,
             size:
                 TerminalSize {
@@ -203,7 +200,6 @@ impl CliListResultItem {
             top_row,
             left_px,
             top_px,
-            font_scale,
             tab_title: tab_title.to_string(),
             window_title: window_title.to_string(),
             is_active: is_active_pane,
