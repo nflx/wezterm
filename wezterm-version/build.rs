fe109feb4c7f20296baa4bf8cdffa1a996b77563
nflx <flux@sys.nu>
2026-08-08 00:30:29 +0200

    Allow explicit versioning of local builds

--- wezterm-version/build.rs
+++ wezterm-version/build.rs
@@ -1,10 +1,13 @@
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=WEZTERM_BUILD_VERSION");
 
     // If a file named `.tag` is present, we'll take its contents for the
     // version number that we report in wezterm -h.
     let mut ci_tag = String::new();
-    if let Ok(tag) = std::fs::read("../.tag") {
+    if let Ok(version) = std::env::var("WEZTERM_BUILD_VERSION") {
+        ci_tag = version;
+    } else if let Ok(tag) = std::fs::read("../.tag") {
         if let Ok(s) = String::from_utf8(tag) {
             ci_tag = s.trim().to_string();
             println!("cargo:rerun-if-changed=../.tag");
